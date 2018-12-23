32e04e9e5166b5b21494c24a3d4397264a6dd8b3
kpilszak <klaudia.pilszak@gmail.com>
2018-12-23 19:14:29 +0100

    Add entry_page() returning entry.html

--- vsearch4web.py
+++ vsearch4web.py
@@ -4,5 +4,7 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 @app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to the search4letters website!')
 
 app.run()
