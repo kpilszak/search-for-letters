f7c384ebb7b1fa1776d8ff23c744858c349bbaa6
kpilszak <klaudia.pilszak@gmail.com>
2018-12-23 20:16:10 +0100

    Correct styling

--- vsearch4web.py
+++ vsearch4web.py
@@ -3,8 +3,12 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+@app.route('/search4', methods=['POST'])
+def do_search() -> str:
+    return str(search4letters('Tekst'))
+
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the search4letters website!')
 
-app.run()
+app.run(debug=True)
