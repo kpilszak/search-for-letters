bca64fd19795a77a61b5c0ef73b189fa269cd19d
kpilszak <klaudia.pilszak@gmail.com>
2019-01-03 22:52:23 +0100

    Correct

--- vsearch4web.py
+++ vsearch4web.py
@@ -15,13 +15,12 @@ def log_request(req: 'flask_request', res: str) -> None:
                     (phrase, letters, ip, browser_string, results)
                     values
                     (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.fomr['phrase'],
+        cursor.execute(_SQL, (req.form['phrase'],
                               req.form['letters'],
-                              reg.remote_addr,
+                              req.remote_addr,
                               req.user_agent.browser,
                               res, ))
-        data = cursor.fetchall()
-
+       
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
