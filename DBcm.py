ab36cefbf101ee3d2abbc7051dbea5a015e97e90
kpilszak <klaudia.pilszak@gmail.com>
2019-01-03 21:59:40 +0100

    Add dunder init

--- DBcm.py
+++ DBcm.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 class UseDatabase:
 
-    def __init__(self):
-        pass
+    def __init__(self, config: dict) -> None:
+        self.configuration = config
 
     def __enter__(self):
         pass
