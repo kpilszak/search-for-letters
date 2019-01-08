88e59b385ff6a03ef92f79e40c1c2610ec44786e
kpilszak <klaudia.pilszak@gmail.com>
2019-01-08 22:17:37 +0100

    Add error handling in vsearch4web.py

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+
+from time import sleep
 
 app = Flask(__name__)
 
@@ -21,6 +23,7 @@ def do_logout() -> str:
     return 'Now you are logged out.'
 
 def log_request(req: 'flask_request', res: str) -> None:
+    sleep(15)
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = """insert into log
                     (phrase, letters, ip, browser_string, results)
@@ -38,7 +41,10 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('***** Login failed; error: ', str(err))
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -53,15 +59,25 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Address', 'User agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View log',
-                           the_row_titles=titles,
-                           the_data=contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Address', 'User agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View log',
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database on? Error: ', str(err))
+    except CredentialsError as err:
+        print('Incorrect login or password. Error: ', str(err))
+    except SQLError as err:
+        print('Is SQL query correct? Error: ', str(err))
+    except Exception as err:
+        print('Something went wrong. Error: ', str(err))
+    return 'Error'
 
 app.secret_key = 'VeryDifficultKeyToGuess'
 
